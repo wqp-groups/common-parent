b2063bae7d0149fcd08015f16c84c9332b1a6b76
zfengfei <907898929@qq.com>
2019-11-04 20:37:32 +0800

    optimize code

--- common-stream/src/main/java/com/wqp/common/stream/TestMqttSchedulingTask.java
+++ common-stream/src/main/java/com/wqp/common/stream/TestMqttSchedulingTask.java
@@ -14,14 +14,13 @@ public class TestMqttSchedulingTask implements SchedulingConfigurer {
     @Override
     public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
         MqttExecutorClient mqttExecutorClient = new MqttExecutorClient();
-        scheduledTaskRegistrar.addTriggerTask(() -> process(mqttExecutorClient), triggerContext -> new CronTrigger("0/3 * * * * ?").nextExecutionTime(triggerContext));
-    }
-
-    private void process(MqttExecutorClient mqttExecutorClient){
         MqttConf mqttConf = new MqttConf();
         mqttConf.setHost("202.116.104.36").setPort(1883).setUsername("aiot_pub_client").setPassword("123456");
         mqttExecutorClient.setMqttConf(mqttConf);
-        mqttExecutorClient.publish("aiot/YK20190808CP001/YK20190808ED001/YK20190808SD004", "{\"title\":\"标题\",\"payload\":\"这是一条手环测试内容\"}");
+        scheduledTaskRegistrar.addTriggerTask(() -> process(mqttExecutorClient), triggerContext -> new CronTrigger("0/5 * * * * ?").nextExecutionTime(triggerContext));
+    }
 
+    private void process(MqttExecutorClient mqttExecutorClient){
+        mqttExecutorClient.publish("aiot/YK20190808CP001/YK20190808ED001/YK20190808SD004", "{\"title\":\"标题\",\"payload\":\"这是一条手环测试内容\"}");
     }
 }
