8ac82c700e74eb5d083f9d3957c55cc292700c96
zfengfei <907898929@qq.com>
2019-11-05 13:49:05 +0800

    optimize code

--- common-stream/src/main/java/com/wqp/common/stream/TestMqttSchedulingTask.java
+++ common-stream/src/main/java/com/wqp/common/stream/TestMqttSchedulingTask.java
@@ -1,5 +1,7 @@
 package com.wqp.common.stream;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wqp.common.stream.mqtt.client.MqttExecutorClient;
 import com.wqp.common.stream.mqtt.core.MqttConf;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,6 +13,9 @@ import org.springframework.stereotype.Component;
 @Component
 @EnableScheduling
 public class TestMqttSchedulingTask implements SchedulingConfigurer {
+    private int sequence = 1;
+    private ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
         MqttExecutorClient mqttExecutorClient = new MqttExecutorClient();
@@ -21,6 +26,37 @@ public class TestMqttSchedulingTask implements SchedulingConfigurer {
     }
 
     private void process(MqttExecutorClient mqttExecutorClient){
-        mqttExecutorClient.publish("aiot/YK20190808CP001/YK20190808ED001/YK20190808SD004", "{\"title\":\"标题\",\"payload\":\"这是一条手环测试内容\"}");
+        sequence += 1;
+        MqttPayload mqttPayload = new MqttPayload();
+        mqttPayload.setTitle("标题"+sequence);
+        mqttPayload.setPayload("这是一条手环测试内容"+sequence);
+        String payload = null;
+        try {
+            payload = objectMapper.writeValueAsString(mqttPayload);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        if(payload != null) mqttExecutorClient.publish("aiot/YK20190808CP001/YK20190808ED001/YK20190808SD004", payload);
+    }
+
+    private class MqttPayload{
+        private String title;
+        private String payload;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getPayload() {
+            return payload;
+        }
+
+        public void setPayload(String payload) {
+            this.payload = payload;
+        }
     }
 }
